0e1231c4e9111db7a082d2fff941539918a2ca0f
bigtank <codeplay.erniie@gmail.com>
2023-07-07 03:41:24 +0100

    added mobile slider effects

--- public/js/mobile-menu.js
+++ public/js/mobile-menu.js
@@ -2,12 +2,15 @@ export function mobileMenu() {
   let block = document.querySelector('header.header');
 
   if (block) {
+    const body = document.querySelector('body');
     const hamburger = block.querySelector(".hamburger");
-    const menu = block.querySelector('.menu ul');
+    const mobileMenu = block.querySelector('.mobile-menu');
     
     hamburger.addEventListener("click", function() {
       hamburger.classList.toggle("is-active");
-      menu.classList.toggle("active");
+      block.classList.toggle("mobile-menu-active");
+      mobileMenu.classList.toggle("active");
+      body.classList.toggle("scroll-lock");
     });
   }
 
