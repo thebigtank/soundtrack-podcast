b94d0e0758c0ce8b80fa4fe5af4d94e1fdd96eab
bigtank <codeplay.erniie@gmail.com>
2023-07-09 00:45:43 +0100

    cleaned and deleted unused files/codes

--- public/main.js
+++ public/main.js
@@ -1,14 +1,10 @@
 import './scss/main.scss';
 import { mobileMenu } from './js/mobile-menu.js'
 import { revealNav } from './js/reveal-nav.js'
-import { siteAnimations } from './js/animations/site-animations.js'
 import { rangeInput } from './js/range-input.js'
-import { customCursor } from './js/custom-cursor.js'
 
 document.addEventListener("DOMContentLoaded", function () {
 	mobileMenu();
 	revealNav();
 	rangeInput();
-	// siteAnimations();
-	// customCursor();
 });
